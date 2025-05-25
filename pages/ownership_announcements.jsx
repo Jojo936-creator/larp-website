13ab353439278776c1e5500b5196f814b47ad604
Jojo936-creator <jojo530278@gmail.com>
2025-05-25 15:05:15 +0200

    Update ownership_announcements.jsx

--- pages/ownership_announcements.jsx
+++ pages/ownership_announcements.jsx
@@ -1,11 +1,11 @@
 import AnnouncementsPage from '../components/AnnouncementsPage';
 import { requireAuth } from '../lib/auth';
 
-export default function StaffAnnouncements({ user }) {
+export default function OwnershipAnnouncements({ user }) {
   return <AnnouncementsPage user={user} channel="ownership" />;
 }
 
 export async function getServerSideProps(context) {
-  return requireAuth['owner', 'superowner'](context);
+  return requireAuth(['owner', 'superowner'])(context);
 }
 
